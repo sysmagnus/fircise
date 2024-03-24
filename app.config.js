29509d6a811c164c0099322d5b4a6a937328e6b9
Nimrod León <nleonc14@gmail.com>
2024-03-24 00:36:57 -0500

    add: firebase installed

---
+++ app.config.js
@@ -0,0 +1,40 @@
+import 'dotenv/config'
+
+export default {
+    "expo": {
+        "name": "fircise",
+        "slug": "fircise",
+        "version": "1.0.0",
+        "orientation": "portrait",
+        "icon": "./assets/icon.png",
+        "userInterfaceStyle": "light",
+        "splash": {
+            "image": "./assets/splash.png",
+            "resizeMode": "contain",
+            "backgroundColor": "#ffffff"
+        },
+        "assetBundlePatterns": [
+            "**/*"
+        ],
+        "ios": {
+            "supportsTablet": true
+        },
+        "android": {
+            "adaptiveIcon": {
+                "foregroundImage": "./assets/adaptive-icon.png",
+                "backgroundColor": "#ffffff"
+            }
+        },
+        "web": {
+            "favicon": "./assets/favicon.png"
+        },
+        "extra": {
+            apiKey: process.env.APIKEY,
+            authDomain: process.env.AUTHDOMAIN,
+            projectId: process.env.PROJECTID,
+            storageBucket: process.env.STORAGEBUCKET,
+            messagingSenderId: process.env.MESSAGINGSENDERID,
+            appId: process.env.APPID,
+        }
+    }
+}
