eb65d29f77f46b6d5ceaa46a3372b09aae4b0da0
Nimrod León <nleonc14@gmail.com>
2024-04-02 22:46:00 -0500

    fix: notification

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import { enableLatestRenderer } from 'react-native-maps'
 import { Navigation } from './navigation'
 import { NativeBaseProvider } from 'native-base'
 import { AuthenticatedUserProvider } from './context'
+import * as Notifications from "expo-notifications"
 
 enableLatestRenderer()
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+})
 
 export default function App() {
     return (
